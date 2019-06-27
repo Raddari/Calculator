5d8c621cbccbea2239a422426ba2b3e24d5ef45d
Raddari <harveywyatt@hotmail.com>
2019-06-27 14:28:25 +0930

    Fixed main

--- src/main/java/me/harvey/calculator/Calculator.java
+++ src/main/java/me/harvey/calculator/Calculator.java
@@ -7,7 +7,7 @@ package me.harvey.calculator;
  */
 public final class Calculator{
 	
-	public void main(String[] args) {
+	public static void main(String[] args) {
 		System.out.println("Hello, world!");
 	}
 	

abd5f29b6b3021d0ddac560081b640c37ddb51c4
Raddari <harveywyatt@hotmail.com>
2019-06-27 16:56:54 +0930

    Added StringUtil with isNumeric method

---
+++ src/main/java/me/harvey/calculator/StringUtil.java
@@ -0,0 +1,17 @@
+package me.harvey.calculator;
+
+import org.jetbrains.annotations.NotNull;
+
+public final class StringUtil {
+	
+	/**
+	 * Checks if a String contains only numeric characters (with optional negative sign and decimal).
+	 * This ONLY works for latin numbers 0-9.
+	 * @param str the string to check
+	 * @return {@code true} if the string comprises of only numbers
+	 */
+	public static boolean isNumeric(@NotNull String str) {
+		return str.matches("-?\\d+(\\.\\d+)?");
+	}
+	
+}
